d2a7c39e859ac6778405b2114e8c88f3295c8890
Nhat <hiamnhatdz203@gmail.com>
2024-07-24 21:32:27 +0700

    feat: initn

--- app/src/main/java/com/nhatle/base_kotlin/TextView/LearnTextView.kt
+++ app/src/main/java/com/nhatle/base_kotlin/TextView/LearnTextView.kt
@@ -13,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.nhatle.base_kotlin.R
 
 class LearnTextView : AppCompatActivity(), View.OnClickListener {
+   private lateinit var text_msg:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,7 +24,7 @@ class LearnTextView : AppCompatActivity(), View.OnClickListener {
             insets
         }
 
-        val text_msg:TextView = findViewById(R.id.txt_msg)
+        text_msg = findViewById(R.id.txt_msg)
         val text_msg2:TextView = findViewById(R.id.txt_msg2)
 //        text_msg.setOnClickListener { view ->
 //            val goodbyeMsg = getString(R.string.text_goodbye)
